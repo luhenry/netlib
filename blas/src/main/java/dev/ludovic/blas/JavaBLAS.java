1ebe2d494f625dbb8690692839643b4bf22550a0
Ludovic Henry <git@ludovic.dev>
2020-12-31 09:22:32 +0000

    Add dsyr and ssyr implementations

--- blas/src/main/java/dev/ludovic/blas/JavaBLAS.java
+++ blas/src/main/java/dev/ludovic/blas/JavaBLAS.java
@@ -1246,7 +1246,58 @@ public class JavaBLAS implements BLAS {
 
   // a += alpha * x * x.t
   public void dsyr(String uplo, int n, double alpha, double[] x, int offsetx, int incx, double[] a, int offseta, int lda) {
-    f2j.dsyr(uplo, n, alpha, x, offsetx, incx, a, offseta, lda);
+    if (!lsame("U", uplo) && !lsame("L", uplo)) {
+      throw illegalArgument("DSYR", 1);
+    }
+    if (n < 0) {
+      throw illegalArgument("DSYR", 2);
+    }
+    if (incx == 0) {
+      throw illegalArgument("DSYR", 5);
+    }
+    if (lda < Math.max(1, n)) {
+      throw illegalArgument("DSYR", 7);
+    }
+    if (n == 0) {
+      return;
+    }
+    if (alpha != 0.0) {
+      if (lsame("U", uplo)) {
+        int col = 0, ix = incx < 0 ? (n - 1) * -incx : 0;
+        for (; col < loopBound(n, 4); col += 4, ix += incx * 4) {
+          double alphaix0 = alpha * x[offsetx + ix + incx * 0];
+          double alphaix1 = alpha * x[offsetx + ix + incx * 1];
+          double alphaix2 = alpha * x[offsetx + ix + incx * 2];
+          double alphaix3 = alpha * x[offsetx + ix + incx * 3];
+          int row = 0, jx = incx < 0 ? col * -incx : 0;
+          for (; row < col + 1; row += 1, jx += incx) {
+            double xjx = x[offsetx + jx];
+            a[offseta + row + (col + 0) * lda] += alphaix0 * xjx;
+            a[offseta + row + (col + 1) * lda] += alphaix1 * xjx;
+            a[offseta + row + (col + 2) * lda] += alphaix2 * xjx;
+            a[offseta + row + (col + 3) * lda] += alphaix3 * xjx;
+          }
+          double xjx0 = x[offsetx + jx + incx * 0];
+          a[offseta + (row + 0) + (col + 1) * lda] += alphaix1 * xjx0;
+          a[offseta + (row + 0) + (col + 2) * lda] += alphaix2 * xjx0;
+          a[offseta + (row + 0) + (col + 3) * lda] += alphaix3 * xjx0;
+          double xjx1 = x[offsetx + jx + incx * 1];
+          a[offseta + (row + 1) + (col + 2) * lda] += alphaix2 * xjx1;
+          a[offseta + (row + 1) + (col + 3) * lda] += alphaix3 * xjx1;
+          double xjx2 = x[offsetx + jx + incx * 2];
+          a[offseta + (row + 2) + (col + 3) * lda] += alphaix3 * xjx2;
+        }
+        for (; col < n; col += 1, ix += incx) {
+          double alphaix = alpha * x[offsetx + ix];
+          int row = 0, jx = incx < 0 ? col * -incx : 0;
+          for (; row < col + 1; row += 1, jx += incx) {
+            a[offseta + row + col * lda] += alphaix * x[offsetx + jx];
+          }
+        }
+      } else {
+        f2j.dsyr(uplo, n, alpha, x, offsetx, incx, a, offseta, lda);
+      }
+    }
   }
 
   public void ssyr(String uplo, int n, float alpha, float[] x, int incx, float[] a, int lda) {
@@ -1254,7 +1305,58 @@ public class JavaBLAS implements BLAS {
   }
 
   public void ssyr(String uplo, int n, float alpha, float[] x, int offsetx, int incx, float[] a, int offseta, int lda) {
-    f2j.ssyr(uplo, n, alpha, x, offsetx, incx, a, offseta, lda);
+    if (!lsame("U", uplo) && !lsame("L", uplo)) {
+      throw illegalArgument("SSYR", 1);
+    }
+    if (n < 0) {
+      throw illegalArgument("SSYR", 2);
+    }
+    if (incx == 0) {
+      throw illegalArgument("SSYR", 5);
+    }
+    if (lda < Math.max(1, n)) {
+      throw illegalArgument("SSYR", 7);
+    }
+    if (n == 0) {
+      return;
+    }
+    if (alpha != 0.0) {
+      if (lsame("U", uplo)) {
+        int col = 0, ix = incx < 0 ? (n - 1) * -incx : 0;
+        for (; col < loopBound(n, 4); col += 4, ix += incx * 4) {
+          float alphaix0 = alpha * x[offsetx + ix + incx * 0];
+          float alphaix1 = alpha * x[offsetx + ix + incx * 1];
+          float alphaix2 = alpha * x[offsetx + ix + incx * 2];
+          float alphaix3 = alpha * x[offsetx + ix + incx * 3];
+          int row = 0, jx = incx < 0 ? col * -incx : 0;
+          for (; row < col + 1; row += 1, jx += incx) {
+            float xjx = x[offsetx + jx];
+            a[offseta + row + (col + 0) * lda] += alphaix0 * xjx;
+            a[offseta + row + (col + 1) * lda] += alphaix1 * xjx;
+            a[offseta + row + (col + 2) * lda] += alphaix2 * xjx;
+            a[offseta + row + (col + 3) * lda] += alphaix3 * xjx;
+          }
+          float xjx0 = x[offsetx + jx + incx * 0];
+          a[offseta + (row + 0) + (col + 1) * lda] += alphaix1 * xjx0;
+          a[offseta + (row + 0) + (col + 2) * lda] += alphaix2 * xjx0;
+          a[offseta + (row + 0) + (col + 3) * lda] += alphaix3 * xjx0;
+          float xjx1 = x[offsetx + jx + incx * 1];
+          a[offseta + (row + 1) + (col + 2) * lda] += alphaix2 * xjx1;
+          a[offseta + (row + 1) + (col + 3) * lda] += alphaix3 * xjx1;
+          float xjx2 = x[offsetx + jx + incx * 2];
+          a[offseta + (row + 2) + (col + 3) * lda] += alphaix3 * xjx2;
+        }
+        for (; col < n; col += 1, ix += incx) {
+          float alphaix = alpha * x[offsetx + ix];
+          int row = 0, jx = incx < 0 ? col * -incx : 0;
+          for (; row < col + 1; row += 1, jx += incx) {
+            a[offseta + row + col * lda] += alphaix * x[offsetx + jx];
+          }
+        }
+      } else {
+        f2j.ssyr(uplo, n, alpha, x, offsetx, incx, a, offseta, lda);
+      }
+    }
   }
 
   public void dsyr2(String uplo, int n, double alpha, double[] x, int incx, double[] y, int incy, double[] a, int lda) {
---
+++ blas/src/test/java/dev/ludovic/blas/l2/SsyrTest.java
@@ -0,0 +1,54 @@
+/*
+* Copyright 2020, Ludovic Henry
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+import dev.ludovic.blas.BLAS;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SsyrTest extends BLASTest {
+
+    @ParameterizedTest
+    @MethodSource("BLASImplementations")
+    void testSanity(BLAS blas) {
+        int n = 4;
+        float alpha = 0.15f;
+        float[] x = new float[] {
+            0.0f, 2.7f, 3.5f, 2.1f };
+        float[] a = new float[] {
+            0.0f, 1.2f, 2.2f, 3.1f,
+            1.2f, 3.2f, 5.3f, 4.6f,
+            2.2f, 5.3f, 1.8f, 3.0f,
+            3.1f, 4.6f, 3.0f, 0.8f };
+        float[] expected = new float[] {
+            0.0f,    1.2f,    2.2f,       3.1f,
+            1.2f, 4.2935f,    5.3f,       4.6f,
+            2.2f, 6.7175f, 3.6375f,       3.0f,
+            3.1f, 5.4505f, 4.1025f, 1.4614999f };
+
+        float[] a1 = a.clone();
+        blas.ssyr("U", n, alpha, x, 1, a1, n);
+        assertArrayEquals(expected, a1);
+    }
+}
