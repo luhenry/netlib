96d15fea3168a38ed599df1540ee53acb6ce002c
Ludovic Henry <git@ludovic.dev>
2021-04-30 18:02:16 +0200

    Make sure license is consistent across all files

--- lapack/src/test/java/dev/ludovic/netlib/lapack/DppsvTest.java
+++ lapack/src/test/java/dev/ludovic/netlib/lapack/DppsvTest.java
@@ -18,6 +18,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * Please contact git@ludovic.dev or visit ludovic.dev if you need additional
+ * information or have any questions.
  */
 
 import dev.ludovic.netlib.LAPACK;
