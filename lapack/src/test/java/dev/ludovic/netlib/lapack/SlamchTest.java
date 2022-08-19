67d778491e3cefd4f44abbbeeec49ae53325135f
Ludovic Henry <git@ludovic.dev>
2022-08-19 22:39:32 +0200

    fixup! Add missing tests

--- lapack/src/test/java/dev/ludovic/netlib/lapack/SlamchTest.java
+++ lapack/src/test/java/dev/ludovic/netlib/lapack/SlamchTest.java
@@ -30,7 +30,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SlanchTest extends LAPACKTest {
+public class SlamchTest extends LAPACKTest {
 
     @ParameterizedTest
     @MethodSource("LAPACKImplementations")
